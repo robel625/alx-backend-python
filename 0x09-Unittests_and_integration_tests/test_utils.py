ffc8435275123e3f736b6f11672504f56cfd9842
robel625 <76595851+robel625@users.noreply.github.com>
2022-07-17 02:20:02 +0300

    Update test_utils.py

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-""" test file """
-import unittest
-from unittest.mock import patch
-from parameterized import parameterized
+""" Test SUITE Unittest module Task """
 
+from unittest import TestCase, mock
+from unittest.mock import patch, Mock
+from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
-class TestAccessNestedMap(unittest.TestCase):
-    """ Class test nested map """
+
+class TestAccessNestedMap(TestCase):
+    """ Class for testing Nested Map function """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, expected_output):
-        """ returns correct output """
+        """ Test method return output """
         real_output = access_nested_map(map, path)
         self.assertEqual(real_output, expected_output)
 
@@ -23,45 +24,54 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), 'b')
     ])
     def test_access_nested_map_exception(self, map, path, wrong_output):
-        """ raises correct exception """
-
+        """ Test method raises correct exception """
         with self.assertRaises(KeyError) as e:
             access_nested_map(map, path)
+            self.assertEqual(wrong_output, e.exception)
 
-        self.assertEqual(str(e.exception)[1:-1], wrong_output)
 
-
-class TestGetJson(unittest.TestCase):
-    """ Test with json """
+class TestGetJson(TestCase):
+    """ Class for testing get_json function """
+    # order of args: test_url, test_payload
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
+        ("http://holberton.io", {"payload": False})
     ])
-    @patch('test_utils.get_json')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        """ test utils.get_json """
-        mock_get.return_value = test_payload
-        result = get_json(test_url)
-        self.assertEqual(result, test_payload)
+    def test_get_json(self, test_url, test_payload):
+        """ Test method returns correct output """
+        # set mock response to have return value of test payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        # function calls requests.get, need patch to get mock return value
+        with patch('requests.get', return_value=mock_response):
+            real_response = get_json(test_url)
+            self.assertEqual(real_response, test_payload)
+            # check that mocked method called once per input
+            mock_response.json.assert_called_once()
 
 
-class TestMemoize(unittest.TestCase):
-    """ test with memoize """
+class TestMemoize(TestCase):
+    """ Class for testing memoization """
+
     def test_memoize(self):
-        """ Ttest assert_called_once
-        """
+        """ Tests memoize function """
+
         class TestClass:
-            """ class """
+            """ Test class """
+
             def a_method(self):
-                """ method  """
+                """ Method to always return 42 """
                 return 42
 
             @memoize
             def a_property(self):
-                """ property """
+                """ Returns memoized property """
                 return self.a_method()
-        with patch.object(TestClass, "a_method") as mockMethod:
+
+        with patch.object(TestClass, 'a_method', return_value=42) as patched:
             test_class = TestClass()
-            test_class.a_property
-            test_class.a_property
-            mockMethod.assert_called_once
+            real_return = test_class.a_property
+            real_return = test_class.a_property
+
+            self.assertEqual(real_return, 42)
+            patched.assert_called_once()
