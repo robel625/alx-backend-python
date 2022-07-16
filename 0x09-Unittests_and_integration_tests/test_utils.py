760eddf0e06f5746e0b13d66b43b3b5e368a101b
robel625 <76595851+robel625@users.noreply.github.com>
2022-07-17 02:16:47 +0300

    Update test_utils.py

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,55 +1,54 @@
 #!/usr/bin/env python3
-""" Parameterize a unit test, Mock HTTP calls, Parameterize and patch """
+""" test file """
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
+
 from utils import access_nested_map, get_json, memoize
 
-
 class TestAccessNestedMap(unittest.TestCase):
-    """ TESTCASE """
-    """ to test the function for following inputs """
+    """ Class test nested map """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, answer):
-        """ method to test that the method returns what it is supposed to """
-        self.assertEqual(access_nested_map(nested_map, path), answer)
+    def test_access_nested_map(self, map, path, expected_output):
+        """ returns correct output """
+        real_output = access_nested_map(map, path)
+        self.assertEqual(real_output, expected_output)
 
-    """  to test that a KeyError is raised for the following inputs """
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        """ method to test that a KeyError is raised properly """
-        with self.assertRaises(KeyError) as error:
-            access_nested_map(nested_map, path)
-        self.assertEqual(error.exception.args[0], path[-1])
+    def test_access_nested_map_exception(self, map, path, wrong_output):
+        """ raises correct exception """
+
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(map, path)
+
+        self.assertEqual(str(e.exception)[1:-1], wrong_output)
 
 
 class TestGetJson(unittest.TestCase):
-    """ TESTCASE """
-    """ to test the function for following inputs """
+    """ Test with json """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     @patch('test_utils.get_json')
     def test_get_json(self, test_url, test_payload, mock_get):
-        """ method to test that utils.get_json returns the expected result """
+        """ test utils.get_json """
         mock_get.return_value = test_payload
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
 
 
 class TestMemoize(unittest.TestCase):
-    """ TESTCASE """
+    """ test with memoize """
     def test_memoize(self):
-        """ Test that when calling a_property twice, the correct result is
-            returned but a_method is only called once using assert_called_once
+        """ Ttest assert_called_once
         """
         class TestClass:
             """ class """
